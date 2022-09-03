e0d0245a277d5a507321f8b38b7c9c9f8de69c31
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2022-09-04 02:01:19 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,15 @@
+A, B = input().split()
+right_numA = list(A)
+right_numB = list(B)
+wrong_numA = []
+wrong_numB = []
+for i in range(len(right_numA)-1,-1,-1):
+  wrong_numA.append(right_numA[i])
+for j in range(len(right_numB)-1,-1,-1):
+  wrong_numB.append(right_numB[j])
+wrong_numAA=int(''.join(wrong_numA))
+wrong_numBB=int(''.join(wrong_numB))
+if wrong_numAA>wrong_numBB:
+  print(wrong_numAA)
+else:
+  print(wrong_numBB)
