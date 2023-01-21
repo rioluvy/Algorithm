9c6573627899d8773504ac4757257e603c69bc63
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-01-21 15:12:58 +0900

    [Silver II] Title: 최대 힙, Time: 104 ms, Memory: 36992 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,15 +1,19 @@
 import sys
 from heapq import heappop, heappush
-input = sys.stdin.readline
-heap = []
-N = int(input())
 
-for _ in range(N):
-  num = int(input())
-  if num != 0:
-    heappush(heap, (-num,num))
-  else:
-    if heap == []:
-      print(0)
+def Sol():
+  input = sys.stdin.readline
+  heap = []
+  N = int(input())
+  for _ in range(N):
+    num = int(input())
+    if num != 0:
+      heappush(heap, (-num))
     else:
-      print(heappop(heap)[1])
+      if heap == []:
+        print(0)
+      else:
+        print(-heappop(heap))
+
+if __name__ == "__main__":
+  Sol()
