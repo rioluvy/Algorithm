5a6e7d4bb26ff755898956bb9d4046c42f28add7
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-01-25 00:23:08 +0900

    [Gold II] Title: 가운데를 말해요, Time: 192 ms, Memory: 37112 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -1,20 +1,24 @@
 import sys
 import heapq
-input = sys.stdin.readline
-heap1 = []
-heap2 = []
-N = int(input())
-for _ in range(N):
-  num = int(input())
-  if len(heap1) == len(heap2):
-    heapq.heappush(heap1, -num)
-  else:
-    heapq.heappush(heap2,num)
-  
-  if heap1 and heap2 and -heap1[0] > heap2[0]:
-    big = heapq.heappop(heap1)
-    small = heapq.heappop(heap2)
+def Sol():
+  input = sys.stdin.readline
+  heap1 = []
+  heap2 = []
+  N = int(input())
+  for _ in range(N):
+    num = int(input())
+    if len(heap1) == len(heap2):
+      heapq.heappush(heap1, -num)
+    else:
+      heapq.heappush(heap2,num)
+    
+    if heap1 and heap2 and -heap1[0] > heap2[0]:
+      big = heapq.heappop(heap1)
+      small = heapq.heappop(heap2)
 
-    heapq.heappush(heap1,-small)
-    heapq.heappush(heap2,-big)
-  print(-heap1[0])
+      heapq.heappush(heap1,-small)
+      heapq.heappush(heap2,-big)
+    print(-heap1[0])
+
+if __name__ == "__main__":
+  Sol()
