3e2f523fbf878798fc37bba82dab2af0e99d7596
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-05-23 14:22:17 +0900

    [Silver II] Title: 이항 계수 2, Time: 240 ms, Memory: 54360 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,10 @@
+N, K = map(int,input().split())
+dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1,N+1):
+  dp[i][1] = i
+  dp[i][0] = 1
+  dp[i][i] = 1
+for i in range(2,N+1):
+  for j in range(1,i):
+    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007
+print(dp[N][K])
