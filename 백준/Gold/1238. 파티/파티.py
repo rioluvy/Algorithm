d7ef1f6f6a1ca2fa470d1d7dbe60275613349ab8
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-05-27 16:52:28 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,34 +3,32 @@ import heapq
 input = sys.stdin.readline
 INF = sys.maxsize
 N, M, X = map(int,input().split())
-adj = [[] for _ in range(N+1)]
+adj_go = [[] for _ in range(N+1)]
+adj_back = [[] for _ in range(N+1)]
 for _ in range(M):
   s, e, t = map(int,input().split())
-  adj[s].append((e,t))
+  adj_go[s].append((e,t))
+  adj_back[e].append((s,t))
 
-def dijkstra(start, X):
+
+def dijkstra(start, graph):
   queue = []
   heapq.heappush(queue,(0,start))
   distList = [INF] * (N+1)
-  visited = [False] * (N+1)
   distList[start] = 0
   while queue:
     (pathLength, node) = heapq.heappop(queue)
-    if visited[node]:
-      continue
-    visited[node] = True
-    for i in adj[node]:
+    if pathLength > distList[node]: continue
+    for i in graph[node]:
       distance = pathLength + i[1]
       if distList[i[0]] > distance:
         distList[i[0]] = distance
         heapq.heappush(queue,(distance, i[0]))
-  return distList[X]
+  return distList[1:]
 
+dist_go = dijkstra(X,adj_go)
+dist_back = dijkstra(X,adj_back)
 ans = 0
-for i in range(1,N+1):
-  go = dijkstra(i,X)
-  comeBack = dijkstra(X,i)
-  time = go + comeBack
-  if ans < time:
-    ans = time
+for i in range(len(dist_go)):
+  ans = max(dist_go[i] + dist_back[i],ans)
 print(ans)
