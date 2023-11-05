bb610a3b75c6257bd76fa5477ac40a0934669167
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-11-05 20:08:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,24 +1,19 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-
 using namespace std;
 int main(){
-  int n, k;
-  cin >> n >> k;
-  vector<int> weight(n+1);
-  vector<int> value(n+1);
-  vector<vector <int>> dp(n+1, vector<int>(k+1)); 
-  for(int i = 1; i < n+1; i++){
-    cin >> weight[i] >> value[i];
-  }
-
-  for(int i = 1; i < n+1; i++){
-    for(int j = 1; j < k+1; j++){
-      if(j >= weight[i]) dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
-      else dp[i][j] = dp[i-1][j];
+    int n, k;
+    cin >> n >> k;
+    int weight[102];
+    int value[102];
+    int *dp = new int[k+2];
+    for(int i = 1; i < n+1; i++){
+        cin >> weight[i] >> value[i];
     }
-  }
-  cout << dp[n][k];
+
+    for(int i = 1 ; i < n+1; i++){
+        for(int j = k; j >= weight[i]; j--){
+            dp[j] = max(dp[j],dp[j-weight[i]]+value[i]);
+        }
+    }
+    cout << dp[k];
 }
