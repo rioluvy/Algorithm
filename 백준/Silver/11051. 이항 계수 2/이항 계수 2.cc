ee6a6e12bfb9a932273097145b963228b5158510
micros0ft-kr <105778036+micros0ft-kr@users.noreply.github.com>
2023-12-20 22:41:32 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstring>
-#define long long ll
 using namespace std;
 
 int cache[1001][1001];
 int bino(int n, int k){
     if(k==0 || n == k) return cache[n][k] = 1;
-    if(cache[n][k] != -1) return cache[n][k];
+    int &ret = cache[n][k];
+    if(ret != -1) return ret;
     return cache[n][k] = (bino(n-1,k) + bino(n-1,k-1))%10007;
 }
 int main(){
