7ec2dce7d04fed8ea93171dda29a6c62075d9695
rioluvy <105778036+rioluvy@users.noreply.github.com>
2024-06-22 23:53:35 +0900

    [Gold IV] Title: 수 분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1437. 수 분해/수 분해.cc
+++ 백준/Gold/1437. 수 분해/수 분해.cc
@@ -1,25 +1,18 @@
 #include <iostream>
+#define mod 10007
 using namespace std;
 
 int main() {
 	int n;
 	cin >> n;
-	if(n == 0 || n == 1) {
-		cout << n;
-		return 0;
+	int* arr = new int[n+1];
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 3;
+	arr[4] = 4;
+	for(int i = 5; i <= n; i++){
+		arr[i] = (arr[i-3] * 3) % mod;
 	}
-	int mx, re, ans;
-	mx = n/3;re = n%3;
-	ans = 1;
-	if(re == 2) ans *= 2;
-	else if(re == 1) {
-		ans *= 4;
-		mx -= 1;
-	}
-	
-	for(int i = 0; i < mx; i++){
-		ans *= 3;
-		ans %= 10007;
-	}
-	cout << ans;
+	cout << arr[n];
 }
