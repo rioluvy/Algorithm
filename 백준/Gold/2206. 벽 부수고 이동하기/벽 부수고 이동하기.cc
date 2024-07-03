d9c99cdc6f34e846b1a2b73ea99eae2b2770b204
rioluvy <105778036+rioluvy@users.noreply.github.com>
2024-07-03 18:40:44 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 144 ms, Memory: 14688 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+int n,m,x,y,cnt,nx,ny,ans;
+int map[1001][1001];
+int visited[1001][1001][2];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+bool flag = false;
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            scanf("%1d", &map[i][j]);
+        }
+    }
+    queue<tuple<int,int,int>> q;
+    q.push(make_tuple(1,1,0));
+    while(!q.empty()){
+        auto [x,y,cnt] = q.front();
+        q.pop();
+        if(x == n && y == m){
+            ans = visited[x][y][cnt]+1;
+            flag = true;
+            break;
+        }
+        for(int i = 0; i < 4; i++){
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if(nx <= 0 || ny <= 0 || nx > n || ny > m) continue;
+            if(visited[nx][ny][cnt]) continue;
+            if(map[nx][ny] == 0){
+                visited[nx][ny][cnt] = visited[x][y][cnt] + 1;
+                q.push(make_tuple(nx,ny,cnt));
+            }
+            else{
+                if(cnt == 1) continue;
+                visited[nx][ny][cnt+1] = visited[x][y][cnt] + 1;
+                q.push(make_tuple(nx,ny,cnt+1));
+            }
+        }
+    }
+    if(flag) printf("%d", ans);
+    else printf("%d", -1);
+}
