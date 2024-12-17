a3c6a5bff49754f9709e74257ae47fc4307fda42
rioluvy <105778036+rioluvy@users.noreply.github.com>
2024-12-17 18:43:40 +0900

    [Gold II] Title: Range Reconstruction, Time: 32 ms, Memory: 2732 KB -BaekjoonHub

---
+++ 백준/Gold/26974. Range Reconstruction/Range Reconstruction.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#define lint long long
+using namespace std;
+
+lint arr[301][301], ans[301], a, b, tmp, ab;
+int n;
+
+int main() {
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = i; j < n; j++){
+			cin >> arr[i][j-i];
+		}
+	}
+	for(int i = n-2; i >= 0; i--){
+		ans[i] = ans[i+1] + arr[i][1];
+		for(int j = 2; j < n-i; j++){
+			tmp = arr[i][j];
+			a = *max_element(ans+i,ans+i+j+1);
+			b = *min_element(ans+i,ans+i+j+1);
+			ab = a - b;
+			if(ab != tmp){
+				ans[i] = ans[i+1] - arr[i][1];
+			}
+		}
+	}
+	for(int i = 0; i < n; i++){
+		cout << ans[i] << " ";
+	}
+}
