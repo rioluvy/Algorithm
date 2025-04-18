209b6759bceed6b4cdd680d9b2de1b1777063f95
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-04-18 16:14:24 +0900

    [Gold IV] Title: N-Queen, Time: 276 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,28 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int n, ans, col[16], dia1[31], dia2[31];
-
-bool check(int r, int c){
-  if(col[c] || dia1[r+c] || dia2[r-c+n] ) return false;
-  return true;
-}
-
-int trial(int cur){
-  if(cur == n+1) {
-    return ans++;
+int n, ans;
+//ld: 좌상향, rd: 우상향
+void trial(int row, int col, int ld, int rd){
+  if(row == (1 << n) - 1){
+    ans++;
+    return;
   }
-  for(int c = 1; c <= n; c++){
-    if(check(cur, c)){
-      col[c] = dia1[cur+c] = dia2[cur-c+n] = 1;
-      trial(cur+1);
-      col[c] = dia1[cur+c] = dia2[cur-c+n] = 0;
-    }
+
+  //Queen이 가능한 곳이 1
+  int avail = ~(col | ld | rd) & ((1 << n) - 1);
+
+  while(avail){
+    int bit = avail & -avail;
+    avail ^= bit;
+    //퀸을 놓으면 1
+    trial(row|bit, col|bit, (ld | bit) >> 1, (rd | bit) << 1);
   }
-  return ans;
 }
 
 int main(){
   cin >> n;
-  cout << trial(1);
+  trial(0,0,0,0);
+  cout << ans;
 }
