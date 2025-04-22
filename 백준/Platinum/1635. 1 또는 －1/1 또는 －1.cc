f71524ab436d1959de4b9d35b3edabed5d7704ca
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-04-22 13:30:07 +0900

    [Platinum V] Title: 1 또는 -1, Time: 116 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1635. 1 또는 －1/1 또는 －1.cc
+++ 백준/Platinum/1635. 1 또는 －1/1 또는 －1.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int n, m, sum, psum, arr[10001][101];
+int n, m, sum, psum, A[101];
 
 int main(){
   ios_base::sync_with_stdio(0);
@@ -11,8 +11,8 @@ int main(){
   for(int i = 0; i < m; i++){
     sum = 0;
     for(int j = 0; j < n; j++){
-      cin >> arr[i][j];
-      sum += arr[i][j];
+      cin >> A[j];
+      sum += A[j];
     }
 
     for(int j = 0; j < n; j++){
@@ -21,7 +21,7 @@ int main(){
         for(int k = j; k < n; k++) cout << -1 << " ";
         break;
       }
-      else psum += arr[i][j];
+      else psum += A[j];
     }
     psum = 0;
     cout << "\n";
