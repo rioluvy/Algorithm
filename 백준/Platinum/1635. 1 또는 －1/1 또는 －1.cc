057f0967fd3533734288719ca92bee9a2fe33113
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-04-22 13:18:29 +0900

    [Platinum V] Title: 1 또는 -1, Time: 116 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Platinum/1635. 1 또는 －1/1 또는 －1.cc
+++ 백준/Platinum/1635. 1 또는 －1/1 또는 －1.cc
@@ -1,36 +1,29 @@
 #include <iostream>
-#include <vector>
-#define endl "\n"
 using namespace std;
 
-int n, m;
+int n, m, sum, psum, arr[10001][101];
 
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);cout.tie(0);
-	
-	cin >> n >> m;
-	while(m--){
-		int s = 0;
-		vector<int> arr(n);
-		for(int i = 0; i < n; i++){
-			cin >> arr[i];
-			s += arr[i];
-		}
-		if(!s){
-			for(int i = 0; i < n; i++) cout << 1 << " ";
-			cout << endl;
-			continue;
-		}
-		int tmp = 0;
-		for(int i = 0; i < n; i++){
-			tmp += arr[i];
-			if(tmp == s/2){
-				for(int j = 0; j <= i; j++) cout << 1 << " ";
-				for(int j = i+1; j < n; j++) cout << -1 << " ";
-				cout << endl;
-				break;
-			}
-		}
-	}
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);cout.tie(0);
+
+  cin >> n >> m;
+  for(int i = 0; i < m; i++){
+    sum = 0;
+    for(int j = 0; j < n; j++){
+      cin >> arr[i][j];
+      sum += arr[i][j];
+    }
+
+    for(int j = 0; j < n; j++){
+      if(psum == sum/2){
+        for(int k = 0; k < j; k++) cout << 1 << " ";
+        for(int k = j; k < n; k++) cout << -1 << " ";
+        break;
+      }
+      else psum += arr[i][j];
+    }
+    psum = 0;
+    cout << "\n";
+  }
 }
