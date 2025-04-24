723a802935ca57dcead42aed59b5a32562363dad
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-04-24 21:57:50 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 408 ms, Memory: 20452 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char star[3072][6144];
+
+void drawStar(int i, int j, int step){
+  if(step == 3){
+    star[i][j] = '*';
+    star[i+1][j-1] = star[i+1][j+1] = '*';
+    for(int k = 0; k < 5; k++){
+      star[i+2][j-2+k] = '*';
+    }
+    return;
+  }
+  int tmp = step/2;
+  drawStar(i,j,tmp);
+  drawStar(i+tmp,j-tmp,tmp);
+  drawStar(i+tmp,j+tmp,tmp);
+}
+
+int main(){
+  ios_base::sync_with_stdio(0); cout.tie(0);
+  int n, h, w;
+  cin >> n;
+  h = n, w = h*2-1;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < w; j++){
+      star[i][j] = ' ';
+    }
+  }
+
+  drawStar(0,w/2,h);
+
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      cout << star[i][j];
+    }
+    cout << '\n';
+  }
+}
