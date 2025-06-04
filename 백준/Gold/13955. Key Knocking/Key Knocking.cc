0d8ff4f7ec85d5bd15f197b72d06cdfe802c3ea1
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-06-04 21:00:47 +0900

    [Gold II] Title: Key Knocking, Time: 12 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/13955. Key Knocking/Key Knocking.cc
+++ 백준/Gold/13955. Key Knocking/Key Knocking.cc
@@ -1,55 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <bitset>
-#define endl "\n"
 using namespace std;
 
+int n, preset[8] = {1,2,0,1,1,0,2,1};
 string s;
-int n, w, tmp, start[8] = {1,2,0,1,1,0,2,1};
+vector<int> ans;
 
-int cal(string s){
-	int tmpw = 0;
-	for(int i = 0; i < s.length()-1; i++){
-		if(s[i] != s[i+1]) tmpw++;
-	}
-	return tmpw;
+int cal(const string& str){
+  int cnt = 0;
+  for(int i = 0; i < str.size()-1; i++){
+    if(str[i] != str[i+1]) cnt++;
+  }
+  return cnt;
 }
 
-int main() {
-	ios_base::sync_with_stdio(0);cin.tie(0);
-	cin >> s;
-	vector<int> ans;
-	w = cal(s);
-	n = s.length()/3;
-	if(w >= 2*n){
-		cout << 0;
-		return 0;
-	}
-	for(auto &v : s) v -= '0';
-	for(int i = 0; i < s.length(); i+=3){
-		if(i == 0){
-			tmp += (s[0]*4+s[1]*2+s[2]*1);
-			if(start[tmp]) ans.push_back(start[tmp]);
-			continue;
-		}
-		tmp = cal(s.substr(i-1,4));
-		if(tmp >= 2) continue;
-		s[i] = 1-s[i]; s[i+1] = 1-s[i+1];
-		tmp = cal(s.substr(i-1,4));
-		if(tmp >= 2){
-			ans.push_back(i+1);
-			continue;
-		}
-		s[i] = 1-s[i];s[i+1] = 1-s[i+1];
-		s[i+1] = 1-s[i+1]; s[i+2] = 1-s[i+2];
-		tmp = cal(s.substr(i-1,4));
-		if(tmp >= 2){
-			ans.push_back(i+2);
-			continue;
-		}
-		
-	}
-	cout << ans.size() << endl;
-	for(auto k : ans) cout << k << " ";
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> s;
+  int len = s.length();
+  n = len / 3;
+
+  if(cal(s) >= 2*n){
+    cout << 0;
+    return 0;
+  }
+
+  for(char &c: s) c -= '0';
+
+  int firstVal = s[0] * 4 + s[1] * 2 + s[2];
+  if (preset[firstVal]) ans.push_back(preset[firstVal]);
+
+  for(int i = 3; i < len; i += 3){
+    if(cal(s.substr(i-1,4)) >= 2) continue;
+
+    s[i] ^= 1; s[i+1] ^= 1;
+    if(cal(s.substr(i-1,4)) >= 2){
+      ans.push_back(i+1);
+      continue;
+    }
+    s[i] ^= 1; s[i+1] ^= 1;
+
+    s[i + 1] ^= 1; s[i + 2] ^= 1;
+    if(cal(s.substr(i-1,4)) >= 2){
+      ans.push_back(i+2);
+    }
+  }
+
+  cout << ans.size() << "\n";
+  for(int v : ans) cout << v << " ";
 }
