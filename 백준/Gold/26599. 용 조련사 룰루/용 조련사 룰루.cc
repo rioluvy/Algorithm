1f81f82a483ace0917d0a88753c1403ab18be9b7
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-07-27 14:40:50 +0900

    [Gold I] Title: 용 조련사 룰루, Time: 708 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/26599. 용 조련사 룰루/용 조련사 룰루.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#define pii pair<int,int>
+using namespace std;
+
+int n, m;
+pii arr[1000001];
+
+int main(){
+  cin >> n >> m;
+  for(int i = 1; i <= n; i++){
+    int force;
+    cin >> force;
+    arr[i] = {force,i};
+  }
+  if(n == 1){
+    cout << "YES\n" << 1;
+    return 0;
+  }
+  sort(arr+1, arr+n+1);
+  bool flag = true;
+  for(int i = n; i > n-3; i--){
+    if(i == 1) break;
+    if(arr[i].first-arr[i-1].first > m) flag = false;
+  }
+  if(!flag){
+    cout << "NO";
+    return 0;
+  }
+  cout << "YES\n" << arr[n].second << " " << arr[n-1].second << " ";
+  for(int i = 1; i <= n-2; i++){
+    cout << arr[i].second << " ";
+  }
+}
