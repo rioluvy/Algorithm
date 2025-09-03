20b7d632d74654dd1f621cdd2e4e1d02a9e609d2
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-09-03 21:41:58 +0900

    [Platinum II] Title: 아리스, 청소합니다! (Hard), Time: 400 ms, Memory: 57208 KB -BaekjoonHub

--- 백준/Platinum/31399. 아리스， 청소합니다！ （Hard）/아리스， 청소합니다！ （Hard）.cc
+++ 백준/Platinum/31399. 아리스， 청소합니다！ （Hard）/아리스， 청소합니다！ （Hard）.cc
@@ -1,40 +1,43 @@
 #include<iostream>
 #include<vector>
-#include<tuple>
+#include<string>
 #include<numeric>
+#include<tuple>
 #define lint long long
 using namespace std;
 
 int dx[4] = {-1,0,1,0};
 int dy[4] = {0,1,0,-1};
 
-inline int stateID(int x, int y, int d, int n, int m){
-  return (x*m+y)*4+d;
+inline int stateId(int x, int y, int d, int col){
+  return (x*col + y)*4+d;
 }
 
-inline tuple<int,int,int> statePos(int id, int n, int m){
-  int d = id%4; id /= 4;
-  int y = id%m; id /= m;
+inline tuple<int,int,int> statePos(int id, int row, int col){
+  int d = id%4;
+  id /= 4;
+  int y = id%col;
+  id /= col;
   int x = id;
   return {x,y,d};
 }
 
-struct DSU {
+struct DSU{
   vector<int> parent;
   vector<lint> w;
 
-  DSU(int N = 0) {init(N);}
-  void init(int N){
-    parent.resize(N);
-    w.assign(N,0);
+  DSU(int n = 0) {init(n);}
+  void init(int n){
+    parent.resize(n);
+    w.assign(n,0);
     iota(parent.begin(),parent.end(),0);
   }
 
   int find(int x){
-    if(parent[x] == x) return x;
+    if(x == parent[x]) return x;
     int p = parent[x];
     int r = find(p);
-    w[x] += w[p];
+    w[x] += w[p]; // x -> p, p -> r
     parent[x] = r;
     return r;
   }
@@ -46,72 +49,63 @@ struct DSU {
 };
 
 int main(){
-  ios::sync_with_stdio(0); 
-  cin.tie(0);
-
-  int n, m;
-  cin >> n >> m;
-  int sx, sy, sd;
-  cin >> sx >> sy >> sd;
-
-  vector<string> a(n), b(n);
-  for(auto& v:a) cin >> v;
-  for(auto& v:b) cin >> v;
-
-  vector<vector<char>> cleaned(n,vector<char>(m,0));
-
-  const int T = n*m*4;
+  int row,col;
+  cin >> row >> col;
+  int x,y,d;
+  cin >> x >> y >> d;
+  vector<string> a(row);
+  vector<string> b(row);
+  for(auto& v : a) cin >> v;
+  for(auto& v : b) cin >> v;
+  vector<vector<char>> cleaned(row,vector<char>(col,0));
+  const int T = row*col*4;
   DSU uf(T);
 
   auto inside = [&](int x, int y){
-    return (0 <= x && x < n && 0 <= y && y < m);
+    return (0 <= x && x < row && 0 <= y && y < col);
   };
 
-  auto nextState = [&](int x, int y, int d) -> tuple<int,int,int> {
+  auto nextState = [&](int x, int y, int d) -> tuple<int,int,int>{
     int rot = (cleaned[x][y] ? b[x][y] : a[x][y]) - '0';
-    int nd = (d+rot) % 4;
-    int nx = x + dx[nd];
-    int ny = y + dy[nd];
+    int nd = (d+rot)%4;
+    int nx = x+dx[nd];
+    int ny = y+dy[nd];
     return {nx,ny,nd};
   };
 
   auto jumpOver = [&](int x, int y, int d) -> tuple<int,int,int,lint>{
     lint dist = 0;
     while(true){
-      int cur = stateID(x,y,d,n,m);
-      int r = uf.find(cur);
+      int cur = stateId(x,y,d,col);
+      int tmp = uf.find(cur);
       dist += uf.w[cur];
 
-      auto[rx,ry,rd] = statePos(r,n,m);
-      auto [nx,ny,nd] = nextState(rx,ry,rd);
-
+      auto [tmpx,tmpy,tmpd] = statePos(tmp,row,col);
+      auto [nx,ny,nd] = nextState(tmpx,tmpy,tmpd);
       if(!inside(nx,ny)) return make_tuple(-1,-1,-1,-1);
       if(!cleaned[nx][ny]) return make_tuple(nx,ny,nd,dist+1);
 
-      int nxt = stateID(nx,ny,nd,n,m);
-      if(uf.find(nxt) == uf.find(r)){
-        return make_tuple(-1,-1,-1,-1);
-      }
-      uf.link(r,nxt);
+      int nxt = stateId(nx,ny,nd,col);
+      int nxtP = uf.find(nxt);
+      if(nxtP == uf.find(tmp)) return make_tuple(-1,-1,-1,-1);
+      uf.link(tmp,nxt);
       x = nx; y = ny; d = nd;
-      dist++;
+      dist++; 
     }
   };
 
   lint ans = 0;
-  while(inside(sx,sy)){
-    if(cleaned[sx][sy]){
-      auto [nx,ny,nd,dist] = jumpOver(sx,sy,sd);
+  while(inside(x,y)){
+    if(cleaned[x][y]){
+      auto [nx,ny,nd,dist] = jumpOver(x,y,d);
       if(nx < 0) break;
       ans += dist;
-      sx = nx; sy = ny; sd = nd;
+      x = nx; y = ny; d = nd;
     }
-
-    auto [nx,ny,nd] = nextState(sx,sy,sd);
-    cleaned[sx][sy] = 1;
+    auto [nx,ny,nd] = nextState(x,y,d);
+    cleaned[x][y] = 1;
     ans++;
-    sx = nx; sy = ny; sd = nd;
+    x = nx; y = ny; d = nd;
   }
-
   cout << ans;
 }
