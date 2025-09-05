87707a3717b3ad6c9d760afdd688f9b50c2fd18a
rioluvy <105778036+rioluvy@users.noreply.github.com>
2025-09-05 10:59:33 +0900

    [Gold III] Title: 시로코와 은행털기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.cc
+++ 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.cc
@@ -1,38 +1,38 @@
 #include<iostream>
 #include<vector>
 #include<bitset>
-#include<cmath>
 #include<climits>
+#include<cmath>
 using namespace std;
 
-int n, k, x, a, b;
-int arr[81];
-
 int main(){
+  int n,k,x;
   cin >> n >> k >> x;
-  for(int i = 0; i < n; i++) cin >> arr[i] >> b;
+  int a,b,arr[81];
+  for(int i = 0; i < n; i++){
+    cin >> a >> b;
+    arr[i] = a;
+  }
+
   vector<bitset<16001>> cache(k+1);
   cache[0][0] = 1;
-  
   for(int i = 0; i < n; i++){
     for(int j = k-1; j >= 0; j--){
-      cache[j+1] |= (cache[j] << arr[i]);
+      cache[j+1] |= cache[j] << arr[i];
     }
   }
 
   int pivot = k*x/2;
-  int nearest = -1;
   int diff = INT_MAX;
-  for(int i = 0; i < 16001; i++){
+  int m = 0;
+  for(int i = 1; i <= 16000; i++){
     if(cache[k][i]){
-      int tmp = abs(i - pivot);
+      int tmp = abs(pivot-i);
       if(tmp <= diff){
         diff = tmp;
-        nearest = i;
+        m = i;
       }
     }
   }
-
-  int ans = nearest*(k*x-nearest);
-  cout << ans;
+  cout << m*(k*x-m);
 }
