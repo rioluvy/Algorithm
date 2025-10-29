32a9ce8516b3de3f47936f1d987129f64e359c76
AaronYoo <105778036+rioluvy@users.noreply.github.com>
2025-10-30 01:42:36 +0900

    [Gold III] Title: 최대 유량, Time: 0 ms, Memory: 5872 KB -BaekjoonHub

--- 백준/Gold/6086. 최대 유량/최대 유량.cc
+++ 백준/Gold/6086. 최대 유량/최대 유량.cc
@@ -1,63 +1,62 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 #include<queue>
+#define MAX 700
 using namespace std;
 
-int cap[700][700], flow[700][700], visited[700], max_flow;
-vector<int> arr[700];
+char u, v;
+int n, c, flow[MAX][MAX], capa[MAX][MAX], visited[MAX], res;
+vector<int> arr[MAX];
 
-int maxFlow(int start, int end) {
-  while(true){
-    fill(visited, visited+700, -1);
+int max_flow(int start,int end){
+  while(1){
+    fill(visited,visited+MAX,-1);
     queue<int> q;
     q.push(start);
     while(!q.empty()){
-      int u = q.front();
+      u = q.front();
       q.pop();
       for(int i = 0; i < arr[u].size(); i++){
-        int v = arr[u][i];
-        if(visited[v] == -1 && cap[u][v] > flow[u][v]){
+        v = arr[u][i];
+        if(visited[v] == -1 && capa[u][v] - flow[u][v] > 0){
+          visited[v] = u;
           q.push(v);
-          visited[v] = u; // 어디서 왔는지 기록
           if(v == end) break;
         }
       }
     }
     if(visited[end] == -1) break;
-
     int f = 2147483647;
     for(int v = end; v != start; v = visited[v]){
       int p = visited[v];
-      f = min(f,cap[p][v] - flow[p][v]);
+      f = min(f, capa[p][v] - flow[p][v]);
     }
+
     for(int v = end; v != start; v = visited[v]){
       int p = visited[v];
       flow[p][v] += f;
       flow[v][p] -= f;
     }
-    max_flow += f;
+    res += f;
   }
-  return max_flow;
+  return res;
 }
 
 int main(){
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n, c;
-  char u,v;
   cin >> n;
   for(int i = 0; i < n; i++){
     cin >> u >> v >> c;
     if('A' <= u && u <= 'Z') u = u-'A';
-    else u = u - 'a' + 26;
-
+    else u = u -'a' + 26;
     if('A' <= v && v <= 'Z') v = v-'A';
     else v = v - 'a' + 26;
 
-    cap[u][v] += c;
-    cap[v][u] += c;
+    capa[u][v] += c;
+    capa[v][u] += c;
     arr[u].push_back(v);
     arr[v].push_back(u);
   }
-  cout << maxFlow(0,'Z'-'A');
+
+  cout << max_flow(0,'Z'-'A');
 }
