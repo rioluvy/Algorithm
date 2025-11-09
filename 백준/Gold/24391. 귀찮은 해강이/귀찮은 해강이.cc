e9dddd4ea862a406f03659c5dea0ce4fe88c3d41
AaronYoo <105778036+rioluvy@users.noreply.github.com>
2025-11-09 13:10:02 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.cc
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.cc
@@ -4,42 +4,51 @@
 #include<queue>
 #include<stack>
 #include<cstring>
+#include<climits>
+#define pii pair<int,int>
+#define piii pair<int,pair<int,int>>
+#define ll long long
 using namespace std;
 
-int n, m, u, v, table[100001], parent[100001];
+int n, m, parent[100001], table[100001];
 
 int find(int u){
-  if(u == parent[u]) return u;
+  if(parent[u] == u) return u;
   return parent[u] = find(parent[u]);
 }
 
 void link(int u, int v){
   u = find(u);
   v = find(v);
-  if(u==v) return;
-
+  if(u == v) return;
   if(u < v) parent[v] = u;
   else parent[u] = v;
 }
 
 int main(){
   ios_base::sync_with_stdio(0);
-  cin.tie(0);
+  cin.tie(0);cout.tie(0);
+
   cin >> n >> m;
   for(int i = 1; i <= n; i++){
     parent[i] = i;
   }
+  int u,v;
   for(int i = 0; i < m; i++){
     cin >> u >> v;
     link(u,v);
   }
-  for(int i = 1; i <= n; i++) cin >> table[i];
 
-  int start = find(table[1]);
-  int cnt = 0;
-  for(int i = 2; i <= n; i++){
-    if(find(table[i]) != start) cnt++;
-    start = find(table[i]);
+  for(int i = 1; i <= n; i++){
+    cin >> table[i];
   }
-  cout << cnt;
+  int start = find(table[1]);
+  int ans = 0;
+  for(int i = 1; i <= n; i++){
+    if(start != find(table[i])){
+      ans++;
+      start = find(table[i]);
+    }
+  }
+  cout << ans;
 }
